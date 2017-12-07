92057b97c43e1f9bd787a760cec46ed297fc5e1a
Julian Reschke <julian.reschke@gmx.de>
2017-12-07 20:42:34 +0100

    change draft names (not filenames) change anchors to sibling specs away from rfc723*, but keeping seven characters to minimize diffs change dates

--- common-abnf.sh
+++ common-abnf.sh
@@ -15,7 +15,19 @@ do
   $bap/bap -i $bap/core.abnf -n "$i" 
 done | \
 	fgrep -v ", defined in [Part" | \
+	fgrep -v ", defined in [MESSGNG" | \
+	fgrep -v ", defined in [SEMNTCS" | \
+	fgrep -v ", defined in [CONDTNL" | \
+	fgrep -v ", defined in [RANGERQ" | \
+	fgrep -v ", defined in [CACHING" | \
+	fgrep -v ", defined in [AUTHFRM" | \
 	fgrep -v ", defined in [RFC723" | \
 	fgrep -v ", see [RFC723" | \
+	fgrep -v ", see [MESSGNG" | \
+	fgrep -v ", see [SEMNTCS" | \
+	fgrep -v ", see [CONDTNL" | \
+	fgrep -v ", see [RANGERQ" | \
+	fgrep -v ", see [CACHING" | \
+	fgrep -v ", see [AUTHFRM" | \
 	LC_COLLATE=C sort | uniq | \
 	$bap/bap -k -i $bap/core.abnf
