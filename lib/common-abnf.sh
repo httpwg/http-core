8c5639e400a999989972b7d4eba1e9e0908ccd20
Julian Reschke <julian.reschke@gmx.de>
2022-11-01 17:32:31 +0100

    avoid fgrep

--- lib/common-abnf.sh
+++ lib/common-abnf.sh
@@ -14,18 +14,18 @@ for i in "$@"
 do
   $bap/bap -i $bap/core.abnf -L 2 -X rfc7405 -n "$i" 
 done | \
-	fgrep -v ", defined in [Part" | \
-	fgrep -v ", defined in [HTTP" | \
-	fgrep -v ", defined in [CONDTNL" | \
-	fgrep -v ", defined in [RANGERQ" | \
-	fgrep -v ", defined in [CACHING" | \
-	fgrep -v ", defined in [AUTHFRM" | \
-	fgrep -v ", defined in [RFC723" | \
-	fgrep -v ", see [RFC723" | \
-	fgrep -v ", see [HTTP" | \
-	fgrep -v ", see [CONDTNL" | \
-	fgrep -v ", see [RANGERQ" | \
-	fgrep -v ", see [CACHING" | \
-	fgrep -v ", see [AUTHFRM" | \
+	grep -Fv ", defined in [Part" | \
+	grep -Fv ", defined in [HTTP" | \
+	grep -Fv ", defined in [CONDTNL" | \
+	grep -Fv ", defined in [RANGERQ" | \
+	grep -Fv ", defined in [CACHING" | \
+	grep -Fv ", defined in [AUTHFRM" | \
+	grep -Fv ", defined in [RFC723" | \
+	grep -Fv ", see [RFC723" | \
+	grep -Fv ", see [HTTP" | \
+	grep -Fv ", see [CONDTNL" | \
+	grep -Fv ", see [RANGERQ" | \
+	grep -Fv ", see [CACHING" | \
+	grep -Fv ", see [AUTHFRM" | \
 	LC_COLLATE=C sort | uniq | \
 	$bap/bap -k -i $bap/core.abnf -X rfc7405
